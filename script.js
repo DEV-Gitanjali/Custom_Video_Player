eb13c18e9ac792863a09ec09137ebd4d71a07aa4
Dev-Gitanjali <jgitanjali592@gmail.com>
2024-02-23 09:18:08 +0530

    this method for play & pause video button & adding play/pause icon

--- script.js
+++ script.js
@@ -6,21 +6,45 @@
 
 //  play & pause video
 function toggleVideoStatus(){
-    return true;
+
+    if(video.paused){
+        video.play();
+    } else{
+        video.pause();
+    }
+        
 
 }
 
 // update play/pause icon
 function upadatePlayIcon(){
-    return true;
+    if(video.paused){
+        play.innerHTML = '<i class = "fa fa-play fa-2x"></i>';
+    }else{
+        play.innerHTML = '<i class = "fa fa-pause fa-2x"></i>';
+        
+    }
 }
 
 
 // update progress & time stamp
 function upadateProgress(){
-    return true;
+    
 }
  
+
+// set video time to progress 
+function setVideoProgress(){
+    
+}
+
+// stop video 
+function stopVideo(){
+    return true;
+
+}
+
+
 //  event listeners
 
 video.addEventListener('click',toggleVideoStatus);
